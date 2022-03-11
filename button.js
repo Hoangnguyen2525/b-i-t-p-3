5dcbbe3c65b0fc5b47018d3d9922e4dfa7a5797a
Hoangnguyen2525 <99356519+Hoangnguyen2525@users.noreply.github.com>
2022-03-11 15:43:40 +0700

    Update button.js

--- button.js
+++ button.js
@@ -1,12 +1,10 @@
 class buttonAD {
     container = document.createComment('div')
     input = document.createElement('input')
-
     constructor(inputType , value ){
         this.input.type = inputType;
         this.input.value = value
         this.container.appendChild(this.input)
     }
-   
 }
 export {button}
