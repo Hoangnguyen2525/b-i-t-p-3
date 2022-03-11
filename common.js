c8504dc178f3894ee96c4905158a8388cb294c9f
Hoangnguyen2525 <99356519+Hoangnguyen2525@users.noreply.github.com>
2022-03-11 15:44:20 +0700

    Update common.js

--- common.js
+++ common.js
@@ -3,23 +3,17 @@ class inputCommon{
     inputField = document.createElement("div")
       input = document.createElement("input")
      errorMessega = document.createElement("div")
-
      constructor(inputType, placeholder){
         this.input.type = inputType
-        this.input.placeholder = placeholder
-        
-        
+        this.input.placeholder = placeholder       
         this.container.appendChild(this.input)
         this.container.appendChild(this.errorMessega)
      }
-
 getValue = () => {
     return this.input.value;
 }
-
 setErrorMessage = (errMessage) => {
     this.errMessage.innerHTML = errMessage
 }
-
 }
 export { inputCommon }
