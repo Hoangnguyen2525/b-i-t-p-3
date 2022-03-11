5fb3a5bb61962837d2212075ef5f77135842fe8d
Hoangnguyen2525 <99356519+Hoangnguyen2525@users.noreply.github.com>
2022-03-11 15:44:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { login } from "./login";
 import { Registration } from "./Resister";
-
 const app = document.querySelector("#app")
 const setScreen = (container) => {
     app.innerHTML = ""
@@ -8,16 +7,12 @@ const setScreen = (container) => {
     app.appendChild(Register.container)
 
 }
-
 const signUp = document.querySelector(".signup-link")
-     
       signUp.addEventListener("click", ( )=>{
         this.container.classList.add("active");
       });
-
  const login = document.querySelector(".login-link")
       login.addEventListener("click", ( )=>{
         container.classList.remove("active");
       });
-      
 export { setScreen }
