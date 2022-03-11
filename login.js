74f6bd6c162c793d4704c73a3a2e483f4ca19765
Hoangnguyen2525 <99356519+Hoangnguyen2525@users.noreply.github.com>
2022-03-11 15:45:56 +0700

    Update login.js

--- login.js
+++ login.js
@@ -2,45 +2,25 @@ import {inputCommon} from'./common.js'
 import {checkBox} from'./checkbox.js'
 import {buttonAD} from'./button.js'
 import {loginSignup} from'./login-signup.js'
-
-
-
-
-
 class login {
     container = document.createElement("div")
-    title = document.createElement("span")
-
-   
+    title = document.createElement("span")   
     inputEmail = new inputCommon( "email", "Enter your email")
-    inputPassword = new inputCommon("password: ", "Enter your password")
-
-    
+    inputPassword = new inputCommon("password: ", "Enter your password")  
     checkBox = new checkBox("checkbox" , "Remember me" , "logCheck " )
-    ForgotPass = document.createComment('a')
-    
+    ForgotPass = document.createComment('a')   
     Button =  new buttonAD( "button" , "Login Now")
     loginSignup = new loginSignup("Not a member" , "Sign up" )
-
-
   constructor(){
       this.title.innerHTML="Login"
       this.container.appendChild(this.title)
       this.container.appendChild(this.inputEmail.container)
       this.container.appendChild(this.inputPassword.container)
-      this.container.appendChild(this.checkBox.container)
-      
+      this.container.appendChild(this.checkBox.container)   
       this.container.appendChild(ForgotPass)
       this.container.ForgotPass.innerHTML="Forgot password?"
       this.container.appendChild(this.Button.container)
-      this.container.appendChild(this.loginSignup.container)
-      
-      
-      
-      
-      
-      
-      
+      this.container.appendChild(this.loginSignup.container)    
       this.container.classList.add('form login')
       this.inputEmail.inputField.classList.add('input-field')  
       this.inputPassword.inputField.classList.add('input-field')  
@@ -48,7 +28,6 @@ class login {
       this.checkBox.content.classList.add('checkbox-content')
       this.checkBox.input.classList.add('logCheck')
       this.checkBox.label.classList.add('text')
-
       this.container.ForgotPass.classList.add('text')
       this.Button.container.classList.add('input-field' , 'button')
       this.loginSignup.container.classList.add('login-signup')
@@ -57,7 +36,6 @@ class login {
       this.container.Button.value.addEventListener("click", (e) => {
         e.preventDefault()
         const EmailInput = this.inputEmail.getValue();
-
         if (!emailValue) {
             this.inputEmail.setErrorMessage("Email cannot be empty")
         } else {
@@ -67,13 +45,6 @@ class login {
          if (!passwordValue) {
             this.inputPassword.setErrorMessage("Password cannot be empty")
         }else{
-            this.inputPassword.setErrorMessage("")
-    
-    
-        
+            this.inputPassword.setErrorMessage("")      
  }
-
-
-
-
 export{login}
